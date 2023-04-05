e9dc11c7e64940b96095c051610d72b80bf7c916
José <jruedat@unal.edu.co>
2023-04-05 00:49:36 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -6,12 +6,12 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+
 import pandas as pd
 import re
 from datetime import datetime
 
 def clean_data():
-
     dataframe = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
     dataframe.drop_duplicates(inplace = True)
     dataframe.dropna(axis=0, inplace=True)
@@ -28,8 +28,7 @@ def clean_data():
     dataframe['monto_del_credito'] = dataframe['monto_del_credito'].apply(int)
     dataframe['comuna_ciudadano'] = dataframe['comuna_ciudadano'].apply(float)
     dataframe['fecha_de_beneficio'] = dataframe['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
-
     dataframe.dropna(axis=0,inplace=True)
     dataframe.drop_duplicates(inplace = True)
 
-    return dataframe
+    return (dataframe)
